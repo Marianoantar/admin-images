0360799961bf5ce60847d895a7f9bc0be9368117
Mariano Tardugno <marianoantar@gmail.com>
2025-07-02 02:58:53 +0000

    Modificacion final Internet.sh y descargar.sh

--- src/scripts/descargar.sh
+++ src/scripts/descargar.sh
@@ -7,4 +7,45 @@
 # Las imágenes descargadas tienen extensión .jpg.
 # Asegúrese de devolver un valor de salida acorde a la situación.
 
-echo Descarga de imágenes de internet no implementada. && exit 1
+# Recibe un argumento indicando la clase de la imagen
+CLASE=$1
+# URL facilitada por profesores ($CLASE se pasa como parámetro a través de la URL)
+URL=https://tuia-edp.org/random-image?category=$CLASE
+
+
+
+# Para descargar se necesita un nombre para guardar el archivo
+ARCHIVO_IMG=imagen.jpg
+
+# Recomendación profesor: usar wget para descargar imagen (descarga y guarda en ARCHIVO_IMG)
+wget "$URL" -O "$ARCHIVO_IMG"
+
+# Mensajes de salida donde se verifica si ARCHIVO_IMG tiene una imagen o no, con -s.
+if [ $? -ne 0 ] || [ ! -s "$ARCHIVO_IMG" ]; then
+	echo "Error al descargar imagen."
+	rm -f "$ARCHIVO_IMG"
+	exit 1
+fi    
+
+# Para crear un nombre unico se calcula un "HASH" con el comando sha256sum que genera una cadena de caracteres
+HASH=$(sha256sum "$ARCHIVO_IMG")
+HASH=${HASH%% *} # Expansion para que solo se quede con el hash
+
+# Creamos un nombre final combinando con el hash
+ARCHIVO_IMG_FIN="${CLASE}_${HASH}.jpg"
+
+# Renombramos el archivo
+mv "$ARCHIVO_IMG" "$ARCHIVO_IMG_FIN"
+
+# Otra vez chequeamos el archivo renombrado, pero si mv falla.
+if [ $? -ne 0 ]; then
+	echo "Error al guardar imagen."
+	rm -f "$ARCHIVO_IMG"
+	exit 2
+fi
+
+# Mensaje de salida final
+
+echo "Imagen guardada como $ARCHIVO_IMG_FIN"
+exit 0
+
--- src/scripts/internet.sh
+++ src/scripts/internet.sh
@@ -4,4 +4,13 @@
 # Asegúrese de retornar un valor de salida acorde a la situación.
 # Puede que necesite modificar el archivo Dockerfile.
 
-echo Chequeo de Internet no implementado. && exit 1
+
+#echo Chequeo de Internet no implementado. && exit 1
+if ping -c 1 8.8.8.8 > /dev/null; then
+    echo "Conexión a Internet: OK"
+    exit 0
+else
+    echo "Conexión a Internet: FALLIDA"
+    exit 1
+fi
+
